317e6cccaff91bb8eb2c44989e3d6e32e031c71a
kevintorbett <krtorbett@gmail.com>
2020-03-10 14:54:48 -0700

    Update stop_par_poller.sh

--- stop_par_poller.sh
+++ stop_par_poller.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Terminating the Parallel Poller process..."
-ps -ef | grep imr-par-poller | awk '{print $2}' | xargs kill -9 > /dev/null 2>&1
+ps -ef | grep xxx-par-poller | awk '{print $2}' | xargs kill -9 > /dev/null 2>&1
 echo "Waiting for the process to be killed..."
 sleep 2
 echo "Display the Java processes running..."
