85c2ab352c4c1ba5d66e571619c16603898023d9
kevintorbett <krtorbett@gmail.com>
2020-03-10 14:54:15 -0700

    Update start_par_poller.sh

--- start_par_poller.sh
+++ start_par_poller.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# IMR ACES/PIES Batch (Parallel) Processing Poller script
-# Authors: imruser
+#  Batch (Parallel) Processing Poller script
+
 # chkconfig: 2345 25 75
 # description: Starts the IMR Parallel poller
 
@@ -8,10 +8,10 @@
 IP=`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`
 
 # Start the IMR Parallel Poller processes.
-echo $"Starting IMR Parallel pollers..."
+echo $"Starting  Parallel pollers..."
 
 # Parallel poller
-sudo -u imruser nohup java  -Dlog4j.configuration=file:///opt/imr-batch/conf/par_poller_log4j.properties -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9012 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=$IP -Xms1024m -jar /opt/imr-batch/imr-par-poller.jar -f /opt/imr-batch/conf/poller.properties -e /opt/imr-batch/errors  > /dev/null 2>&1 &
+sudo -u xxuser nohup java  -Dlog4j.configuration=file:///opt/imr-batch/conf/par_poller_log4j.properties -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9012 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=$IP -Xms1024m -jar /opt/batch/xxx-par-poller.jar -f /opt/batch/conf/poller.properties -e /opt/imr-batch/errors  > /dev/null 2>&1 &
 
 echo "Display the Java processes running...."
 sleep 2
